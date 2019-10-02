95e56f39a574fe03bb5baa94bd20c94e91c14902
Will Thompson <will@willthompson.co.uk>
2019-10-02 06:34:48 +0100

    READMEs

--- wjt-toolbox/README.md
+++ wjt-toolbox/README.md
@@ -3,8 +3,11 @@ Development Containers for Endless OS
 
 Not usable unless you are on the Endless VPN.
 
-```
-podman build -t wjt-toolbox:latest .
-toolbox create -i localhost/wjt-toolbox:latest
-toolbox enter --container wjt-toolbox-latest
+```bash
+# latest
+./build.sh
+# 3.7
+./build.sh 3.7
+# podman args
+./build.sh latest --pull-always
 ```
--- wjt-toolbox/build.sh
+++ wjt-toolbox/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -ex
-BRANCH=${1:-latest}
-podman build --build-arg BRANCH="${BRANCH}" --tag wjt-toolbox $(dirname "${BASH_SOURCE[0]}")
-toolbox rm wjt-toolbox-latest || echo "never mind then"
+BRANCH=${1:latest}
+shift
+
+podman build --build-arg BRANCH="${BRANCH}" --tag wjt-toolbox "$@" $(dirname "${BASH_SOURCE[0]}")
+toolbox rm wjt-toolbox-latest 2>/dev/null || :
 toolbox create -i localhost/wjt-toolbox:"$BRANCH"
 
