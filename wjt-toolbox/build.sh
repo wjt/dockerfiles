1e49854a617af8a5cd746ddb2c4d6131a26f5ae5
Will Thompson <wjt@endlessm.com>
2021-02-23 21:29:09 +0000

    Rework for eos master
    We don't have the docker → podman symlink any more.
    
    But we *do* have the full generality of Debian Bullseye packages at our
    fingertips!

--- wjt-toolbox/build.sh
+++ wjt-toolbox/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
-if [ $# -gt 1 ]; then
+if [ $# -ge 1 ]; then
   BRANCH=$1
   shift
 else
@@ -9,8 +9,8 @@ fi
 # Ensure the Endless VPN is up.
 # No idempotent version of 'up'? Thanks, nmcli!
 nmcli c up 0bf0fed7-519a-4e78-9d0e-23acb53d010e || :
-podman build --build-arg BRANCH="${BRANCH}" --tag wjt-toolbox "$@" $(dirname "${BASH_SOURCE[0]}")
-podman stop wjt-toolbox-latest || :
-toolbox rm wjt-toolbox-latest || :
+podman build --build-arg BRANCH="${BRANCH}" --tag wjt-toolbox:$BRANCH "$@" $(dirname "${BASH_SOURCE[0]}")
+podman stop wjt-toolbox-$BRANCH || :
+toolbox rm wjt-toolbox-$BRANCH || :
 toolbox create -i localhost/wjt-toolbox:"$BRANCH"
 
