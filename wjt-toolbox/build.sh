952e43697fc0674a5ff7c147a9a6fa7f3183a5a5
Will Thompson <wjt@endlessos.org>
2021-08-25 10:57:21 +0100

    Don't try to bring up Endless VPN
    I made it autoconnect.

--- wjt-toolbox/build.sh
+++ wjt-toolbox/build.sh
@@ -6,9 +6,6 @@ else
   BRANCH=latest
 fi
 
-# Ensure the Endless VPN is up.
-# No idempotent version of 'up'? Thanks, nmcli!
-nmcli c up 0bf0fed7-519a-4e78-9d0e-23acb53d010e || :
 podman build --build-arg BRANCH="${BRANCH}" --tag wjt-toolbox:$BRANCH "$@" $(dirname "${BASH_SOURCE[0]}")
 podman stop wjt-toolbox-$BRANCH || :
 toolbox rm wjt-toolbox-$BRANCH || :
