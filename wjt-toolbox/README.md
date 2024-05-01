09b98fd9016b4fc6f78c769e51b47435fe88ee62
Will Thompson <will@willthompson.co.uk>
2024-05-01 17:03:23 +0100

    Hi!

--- wjt-toolbox/README.md
+++ wjt-toolbox/README.md
@@ -1,14 +1,7 @@
 Development Containers for Endless OS
 =====================================
 
-Not usable unless you are on the Endless VPN.
-
 ```bash
-# latest
-./build.sh
-# 3.7
-./build.sh 3.7
 # podman args after --
 ./build.sh -- --pull-always
-./build.sh latest -- --pull-always
 ```
--- wjt-toolbox/build.sh
+++ wjt-toolbox/build.sh
@@ -1,35 +1,15 @@
 #!/bin/bash -ex
-args=$(getopt -o h -l "branch:,help" -n "$0" -- "$@")
-eval set -- "$args"
-
 BRANCH=latest
-
-while true
-do
-  case "$1" in
-    --branch)
-      shift
-      BRANCH="$1"
-      ADMIN_BRANCH="$1"
-      shift
-      ;;
-
-    -h|--help)
-      cat <<EOF
-Usage: $0 [--branch BRANCH] [-- [podman build args]]
-
---pull is an argument you might want to pass
-EOF
-      exit 0
-      ;;
-    --)
-      shift
-      break
-  esac
-done
-
-podman build --build-arg BRANCH="${BRANCH}" --tag wjt-toolbox:$BRANCH "$@" $(dirname "${BASH_SOURCE[0]}")
-podman stop wjt-toolbox-$BRANCH || :
-toolbox rm wjt-toolbox-$BRANCH || :
-toolbox create -i localhost/wjt-toolbox:"$BRANCH"
+IMAGE=wjt-toolbox:$BRANCH
+CONTAINER=${IMAGE/:/-}
+podman build --tag "$IMAGE" "$@" $(dirname "${BASH_SOURCE[0]}")
+if podman ps -a --filter "name=$CONTAINER" --format "{{.Names}}" | grep -q "$CONTAINER"
+then
+  podman stop "$CONTAINER"
+fi
+if podman container exists "$CONTAINER"
+then
+    toolbox rm "$CONTAINER"
+fi
+toolbox create -i localhost/"$IMAGE"
 
